2a06b388748867fa1531b8f7d38f743c786d0a46
Lesly <p.sarahi@unitec.edu>
2019-11-06 20:45:44 -0600

    Inicio

---
+++ WeatherForecast.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Examen1_Bonnie
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+
+        public string result { get; set; }
+
+     
+    }
+}
